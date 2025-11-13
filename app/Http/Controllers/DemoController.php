aae49a3a16ab2d48873b29fd94bf770ac3a231ff
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-13 08:32:21 +0600

    Laravel Request Header

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    function demoAction(Request $request):array{
-        return $request->input();
+    function demoAction(Request $request):string{
+        $name = $request->header('name');
+        $age = $request->header('age');
+        return "My Name is ${name} and I am ${age} years old.";
     }
 }
