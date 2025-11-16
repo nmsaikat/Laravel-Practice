4036e630f89e3a447817541109c0d4947c591167
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-16 23:52:27 +0600

    Laravel Different Response JSON

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DemoController extends Controller
 {
-    function demoAction(Request $request):array|null|int|bool|string{
-        return array(
+    function demoAction(Request $request):JsonResponse{
+        $code = 300;
+        $content = array(
             'city' => 'New York',
-            'country' => 'USA',
-            'status' => true,
-            'population' => 8419600,
-            'mayor' => null
+            'postcode' => '10001',
         );
+
+        return response()->json($content, $code);
     }
 }
