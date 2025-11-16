c802ca9531434c48bd4da8b8d81c0956cc600ff7
nmsaikat <nmsaikat.cf@gmail.com>
2025-11-16 08:37:45 +0600

    Laravel IP Address And Content Request in Postman

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -6,19 +6,12 @@ use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    function demoAction(Request $request){
-        $photoFile = $request->file('photo');
-        $fileSize = filesize($photoFile);
-        $fileType = filetype($photoFile);
-        $fileOriginalName = $photoFile->getClientOriginalName();
-        $fileTeamName = $photoFile->getFilename();
-        $fileExtension = $photoFile->extension();
-        return array(
-            'file_size' => $fileSize,
-            'file_type' => $fileType,
-            'original_name' => $fileOriginalName,
-            'temp_name' => $fileTeamName,
-            'extension' => $fileExtension
-        ) ;
+    function demoAction(Request $request):bool{
+        if($request->accepts(['text/html'])){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
